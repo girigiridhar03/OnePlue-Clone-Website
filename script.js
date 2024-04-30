5a67d634eee5c92f1ce5adffe7c09a02f318310f
girigiridhar03 <143996339+girigiridhar03@users.noreply.github.com>
2024-04-30 19:20:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// let timeline = gsap.timeline();
+
+// timeline.from("#headings h5",{
+//     opacity: 0,
+//     duration : 0.8,
+//     delay: 0.4,
+//     y:20
+// })
+// timeline.from("#headings h1",{
+//     opacity: 0,
+//     duration : 0.5,
+//     y:20
+// })
+
+// timeline.from("#matter #matter-p",{
+//     opacity: 0,
+//     duration : 0.8,
+//     delay:0.4,
+//     y:20
+// })
+
+// timeline.from("#matter div button",{
+//     opacity: 0,
+//     duration : 0.8,
+//     y:20
+// })
+// timeline.from("#matter div p",{
+//     opacity: 0,
+//     duration : 0.8,
+//     y:20
+// })
